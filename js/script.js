61ab0032dd881bf29bf5a2a2e76d8b1d66654d20
maxinevelasco <47545772+maxinevelasco@users.noreply.github.com>
2019-04-02 13:21:52 +0100

    Changes to menu bar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-<script>
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
+window.onscroll = function() {myFunction()};
 
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
-</script>
